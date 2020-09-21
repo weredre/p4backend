d312db59579bdeeb01023df89c664f87fc0e3e50
Andre Heslop <andreheslop@gmail.com>
2020-09-21 14:45:33 -0400

    updated urls

--- p4backend/urls.py
+++ p4backend/urls.py
@@ -23,7 +23,7 @@ from projectApp import views
 router = routers.DefaultRouter()
 router.register(r'characters', views.CharacterViewSet)
 router.register(r'users', views.UserViewSet)
-# router.register(r'groups', views.GroupViewSet)
+router.register(r'shadowamps', views.ShadowAmpViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
